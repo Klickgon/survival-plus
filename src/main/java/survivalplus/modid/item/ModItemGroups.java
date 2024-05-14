9d0440ce5feb57b33ae43089f935b571fcf52aba
Klickgon <nkneffel@gmail.com>
2024-05-14 13:42:33 +0200

    Custom Spawn Eggs are now in the Spawn Egg Item Group

--- src/main/java/survivalplus/modid/item/ModItemGroups.java
+++ src/main/java/survivalplus/modid/item/ModItemGroups.java
@@ -1,7 +1,9 @@
 package survivalplus.modid.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
@@ -14,7 +16,7 @@ import survivalplus.modid.SurvivalPlus;
 public class ModItemGroups {
     public static final ItemGroup SURVIVAL_PLUS_ITEMGROUP = Registry.register(Registries.ITEM_GROUP, new Identifier(SurvivalPlus.MOD_ID, "survival-plus"),
             FabricItemGroup.builder().displayName(Text.translatable("itemgroup.survival-plus"))
-                    .icon(() -> new ItemStack(Items.FURNACE)).entries((displayContext, entries) -> {
+                    .icon(() -> new ItemStack(Items.MOSSY_STONE_BRICKS)).entries((displayContext, entries) -> {
                         entries.add(ModItems.REEPER_SPAWN_EGG);
                         entries.add(ModItems.SCORCHED_SKELETON_SPAWN_EGG);
                         entries.add(ModItems.BUILDER_ZOMBIE_SPAWN_EGG);
@@ -24,6 +26,30 @@ public class ModItemGroups {
                         entries.add(ModItems.LEAPING_SPIDER_SPAWN_EGG);
                     }).build());
 
+
+
     public static void registerItemGroups(){
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(content -> {
+            content.add(ModItems.REEPER_SPAWN_EGG);});
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(content -> {
+            content.add(ModItems.SCORCHED_SKELETON_SPAWN_EGG);});
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(content -> {
+            content.add(ModItems.BUILDER_ZOMBIE_SPAWN_EGG);});
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(content -> {
+            content.add(ModItems.DIGGING_ZOMBIE_SPAWN_EGG);});
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(content -> {
+            content.add(ModItems.MINER_ZOMBIE_SPAWN_EGG);});
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(content -> {
+            content.add(ModItems.LUMBERJACK_ZOMBIE_SPAWN_EGG);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(content -> {
+            content.add(ModItems.LEAPING_SPIDER_SPAWN_EGG);
+        });
     }
 }
