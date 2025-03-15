8108f1107c8dce88396c9cfc5832ace381091371
Klickgon <nkneffel@gmail.com>
2025-03-15 18:23:25 +0100

    Tuned Custom Mob spawn group sizes and weight

--- src/main/java/survivalplus/modid/world/gen/ModEntityGeneration.java
+++ src/main/java/survivalplus/modid/world/gen/ModEntityGeneration.java
@@ -9,12 +9,12 @@ import survivalplus.modid.entity.ModEntities;
 public class ModEntityGeneration {
 
     public static void addSpawns(){
-        BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld(), SpawnGroup.MONSTER, ModEntities.SCORCHEDSKELETON, 25, 1, 3);
-        BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld(), SpawnGroup.MONSTER, ModEntities.MINERZOMBIE, 40, 1, 3);
-        BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld(), SpawnGroup.MONSTER, ModEntities.LUMBERJACKZOMBIE, 35, 1, 3);
-        BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld(), SpawnGroup.MONSTER, ModEntities.DIGGINGZOMBIE, 30, 1, 3);
-        BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld(), SpawnGroup.MONSTER, ModEntities.BUILDERZOMBIE, 25, 1, 1);
-        BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld(), SpawnGroup.MONSTER, ModEntities.REEPER, 20, 1, 3);
-        BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld(), SpawnGroup.MONSTER, ModEntities.LEAPINGSPIDER, 40, 1, 3);
+        BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld(), SpawnGroup.MONSTER, ModEntities.SCORCHEDSKELETON, 25, 1, 1);
+        BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld(), SpawnGroup.MONSTER, ModEntities.MINERZOMBIE, 35, 1, 2);
+        BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld(), SpawnGroup.MONSTER, ModEntities.LUMBERJACKZOMBIE, 35, 1, 2);
+        BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld(), SpawnGroup.MONSTER, ModEntities.DIGGINGZOMBIE, 35, 1, 2);
+        BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld(), SpawnGroup.MONSTER, ModEntities.BUILDERZOMBIE, 35, 1, 2);
+        BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld(), SpawnGroup.MONSTER, ModEntities.REEPER, 25, 1, 2);
+        BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld(), SpawnGroup.MONSTER, ModEntities.LEAPINGSPIDER, 35, 1, 3);
     }
 }
